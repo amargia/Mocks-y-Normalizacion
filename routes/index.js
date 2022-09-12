59c2261c12b6e5d5e6cf5e4356ddbcb138884ccc
amargia <amarzuqa@gmail.com>
2022-09-12 19:52:18 -0300

    faker

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,15 @@ const router = express.Router();
 const products = require("./productRouter");
 const productsList = require("./productListRouter");
 const home = require("./homeRouter");
+const faker = require("./fakerRouter");
+const productsFaker = require("./productsFakerRouter");
 
 //middlewares
+
 router.use("/productos", products);
 router.use("/lista-productos", productsList);
 router.use("/", home);
+router.use("/api/productos-test", faker);
+router.use("/api/faker-list", productsFaker);
 
 module.exports = router;
